a293590c402007aaaa447d34b4519e450e245da7
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2023-09-15 21:50:45 +0200

    Update reg-errors.mjs

--- src/js/error-messages/reg-errors.mjs
+++ src/js/error-messages/reg-errors.mjs
@@ -8,34 +8,32 @@
  * ```
  */
 export function registrationError(response) {
+  const registerCard = document.querySelector("#register-card");
+  
   if (!response.ok) {
-    const registerCard = document.querySelector("#register-card");
-    registerCard.innerHTML = "";
-    registerCard.innerHTML = `<h1 class="card-title mt-3 mb-4">Register</h1>
-                                  <p class="card-text mb-4">
-                                    Profile already exists!
-                                  </p>
-                                  <p class="card-text mb-3">
-                                    Please try another account
-                                  </p>
-                                  <a href="./registration.html"
-                                    class="btn btn-primary mt-4 mb-3 px-8 shadow text-uppercase btn-login">
-                                    Try again
-                                  </a>
-                                  `;
-  } else if (response.ok) {
-    const registerCard = document.querySelector("#register-card");
-    registerCard.innerHTML = "";
-    registerCard.innerHTML = `<h1 class="card-title mt-3 mb-4">Success!</h1>
-                                  <p class="card-text mb-2">
-                                    Your account has been created
-                                  </p>
-                                  <p class="card-text mb-3">
-                                    Please proceed to the login section
-                                  </p>
-                                  <a href="./index.html"
-                                    class="btn btn-primary mt-4 mb-3 px-8 shadow text-uppercase btn-login">
-                                    login
-                                  </a>`;
+    registerCard.innerHTML = `
+      <h1 class="card-title mt-3 mb-4">Register</h1>
+      <p class="card-text mb-4">
+        Looks like this profile already exists.
+      </p>
+      <p class="card-text mb-3">
+        Consider using different credentials.
+      </p>
+      <a href="./registration.html" class="btn btn-primary mt-4 mb-3 px-8 shadow text-uppercase btn-login">
+        Try again
+      </a>`;
+  } else {
+    registerCard.innerHTML = `
+      <h1 class="card-title mt-3 mb-4">Success!</h1>
+      <p class="card-text mb-2">
+        Welcome! Your account has been set up.
+      </p>
+      <p class="card-text mb-3">
+        Navigate to the login page to access your account.
+      </p>
+      <a href="./index.html" class="btn btn-primary mt-4 mb-3 px-8 shadow text-uppercase btn-login">
+        Login
+      </a>`;
   }
 }
+
