a94b112d7aa24142dc7e8c41b2a76489fe9e60cf
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2023-09-15 21:51:01 +0200

    Update submitPost-error.mjs

--- src/js/error-messages/submitPost-error.mjs
+++ src/js/error-messages/submitPost-error.mjs
@@ -9,39 +9,35 @@
  */
 export function submitPostError(response) {
   if (!response.ok) {
-    const sendPostForm = document.querySelector("#form-error");
+    const formError = document.querySelector("#form-error");
     formError.classList.remove("d-none");
-    sendPostForm.innerHTML += `
-                                   <p class="card-text mb-1">
-                                    There seems to be some issues with submitting the post
-                                  </p>
-                                  <p class="card-text mb-3">
-                                    Please try again or return later 
-                                  </p>
-                                  `;
+    formError.innerHTML = `
+      <p class="card-text mb-1">
+        An issue occurred while submitting your post.
+      </p>
+      <p class="card-text mb-3">
+        Consider trying again or returning later.
+      </p>`;
   }
 }
 
 /**
- * // Error handler that checks if response was sent correctly
- * @param {*} error // response body when sending post request to server
+ * Error handler that inspects if the response was conveyed accurately.
+ * @param {*} error The response body when dispatching a post request to the server.
  * @example
- * ```js
- * // call the function and pass in the response from the fetch call
- * submitPostFault(error)
- * ```
+ * // Invoke the function and supply the response from the fetch call
+ * submitPostFault(error);
  */
 export function submitPostFault(error) {
   if (error) {
     const formError = document.querySelector("#form-error");
     formError.classList.remove("d-none");
-    formError.innerHTML += `
-                                   <p class="card-text mb-1">
-                                    There seems to be some issues with submitting the post
-                                  </p>
-                                  <p class="card-text mb-3">
-                                    Please try again or return later 
-                                  </p>
-                                  `;
+    formError.innerHTML = `
+      <p class="card-text mb-1">
+        An issue arose while submitting your post.
+      </p>
+      <p class="card-text mb-3">
+        You might want to retry or come back later.
+      </p>`;
   }
 }
