306025d2d55b603957fec5bafb920ccbc69d47b5
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2023-09-15 21:49:51 +0200

    Update registration.mjs

--- src/js/auth/registration.mjs
+++ src/js/auth/registration.mjs
@@ -1,33 +1,26 @@
 import { API_REG_URL } from "./constants.mjs";
 import { register } from "../handlers/register.mjs";
+
 /**
- * function to retrieve values from a form and sends a HTTP POST request to the server
- * the request contains a object that is created from the form.
- * @example
- * ```js
- * // call the function
- * registerUser();
- * ```
+ * function to retrieve values from a form and sends a HTTP POST request to the server.
+ * The request contains an object that is created from the form.
  */
 export function registerUser() {
   const form = document.querySelector("#reg-form");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const formData = event.target;
-    const method = formData.method;
-    const email = formData.email.value;
-    const name = formData.name.value;
-    const password = formData.password.value;
-    const banner = formData.banner.value;
-    const avatar = formData.avatar.value;
-    const profile = {
-      name: name,
-      email: email,
-      password: password,
-      banner: banner,
-      avatar: avatar,
-    };
+    const { 
+      method, 
+      email: { value: email },
+      name: { value: name },
+      password: { value: password },
+      banner: { value: banner },
+      avatar: { value: avatar }
+    } = event.target;
+    
+    const profile = { name, email, password, banner, avatar };
+
     register(API_REG_URL, profile, method);
   });
 }
